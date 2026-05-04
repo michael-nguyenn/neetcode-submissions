8f5dcfd7d3d91476c7b5a85c9adcd9573225d876
Michael Nguyen <104321330+michael-nguyenn@users.noreply.github.com>
2026-05-04 15:37:55 -0400

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = {
+            ')' : '(',
+            ']' : '[',
+            '}' : '{'
+        }
+
+        o_brackets = []
+
+        for c in s:
+            # opening bracket case
+            if c not in brackets:
+                o_brackets.append(c)
+            # closing bracket case
+            else:
+                # empty or mismatch = non valid
+                if len(o_brackets) == 0:
+                    return False
+
+                opening = o_brackets.pop()
+
+                if brackets[c] != opening:
+                    return False
+        
+        return len(o_brackets) == 0
