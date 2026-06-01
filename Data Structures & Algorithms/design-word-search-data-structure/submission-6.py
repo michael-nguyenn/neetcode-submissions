7297c8d2488841d82fcae1dc6a29c7f4017864ae
Michael Nguyen <104321330+michael-nguyenn@users.noreply.github.com>
2026-06-01 16:43:42 -0400

    Add: design-word-search-data-structure - submission-6

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-6.py
@@ -0,0 +1,44 @@
+class TrieNode:
+    def __init__(self, end=False):
+        self.children = {}
+        self.end = end
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for char in word:
+            if char not in cur.children:
+                cur.children[char] = TrieNode()
+            
+            cur = cur.children[char]
+        
+        cur.end = True
+        
+
+    def search(self, word: str) -> bool:
+        return self.search_all(word, self.root)
+
+    def search_all(self, word: str, cur: TrieNode) -> bool:
+
+        if word == "":
+            return cur.end
+
+        for index, char in enumerate(word):
+            if char == ".":
+                for child in cur.children.values():
+                    if self.search_all(word[index+1:], child):
+                        return True
+                
+                return False
+            elif char not in cur.children:
+                return False
+            else:
+                cur = cur.children[char]
+
+        return cur.end 
+        
