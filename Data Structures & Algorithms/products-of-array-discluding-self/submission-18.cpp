6667138138264d644e9ff3c498059ace349d17f8
Michael Nguyen <104321330+michael-nguyenn@users.noreply.github.com>
2026-06-12 14:55:27 -0400

    Add: products-of-array-discluding-self - submission-18

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-18.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) 
+    {
+        std::vector<int> prefix(nums.size());
+        std::vector<int> postfix(nums.size());
+        std::vector<int> res(nums.size());
+        int n = static_cast<int>(nums.size());
+
+        prefix[0] = 1;
+        postfix[nums.size() - 1] = 1;
+
+        for (int i = 1; i < n; i++)
+        {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            postfix[i] = postfix[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            res[i] = prefix[i] * postfix[i];
+        }
+
+        return res;
+    }
+};
