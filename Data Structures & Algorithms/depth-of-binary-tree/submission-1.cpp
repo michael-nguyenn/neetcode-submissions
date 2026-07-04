2c1991d54f22e46cdb8979017443425acaf1befb
Michael Nguyen <104321330+michael-nguyenn@users.noreply.github.com>
2026-07-04 18:01:28 -0400

    Add: depth-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-1.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// so if we're at any given node, do determine the current depth would be the max between
+// the two children, and then you want to return 1 + that to account for the node itself
+
+class Solution 
+{
+public:
+    int maxDepth(TreeNode* root) 
+    {
+        // a null node contributes nothing
+        if (!root) { return 0; }
+
+        int cur_height = maxDepth(root->left);
+        cur_height = max(cur_height, maxDepth(root->right));
+
+        return 1 + cur_height;
+    }
+};
