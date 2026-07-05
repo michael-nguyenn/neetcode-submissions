df6664b974a5a2df1cd496ff7fcf98a97373e836
Michael Nguyen <104321330+michael-nguyenn@users.noreply.github.com>
2026-07-05 13:05:55 -0400

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution 
+{
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) 
+    {
+        int len = static_cast<int>(intervals.size());
+        // zero and one interval are always ok
+        if (len < 2) { return true; }
+
+        auto cmp = [](const Interval& a, const Interval& b) 
+        {
+            if (a.start != b.start) { return a.start < b.start; }
+            else { return a.end < b.end; }
+        };
+        std::sort(intervals.begin(), intervals.end(), cmp);
+
+        // we want to go thru all the intervals and ensure there is no overlap
+        // and overlap would be int1's end is > int2's begin
+        for (int i = 0; i < len - 1; i++)
+        {
+            Interval& first = intervals[i];
+            Interval& second = intervals[i + 1];
+
+            if (first.end > second.start)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
