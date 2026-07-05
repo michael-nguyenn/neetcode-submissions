93b78763caff7a7468d2b3978734b28ab92f14a6
Michael Nguyen <104321330+michael-nguyenn@users.noreply.github.com>
2026-07-05 13:32:44 -0400

    Add: same-binary-tree - submission-5

---
+++ Data Structures & Algorithms/same-binary-tree/submission-5.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// at a given node provided everything else matches, then the work we must do at a given node
+// is comparing the values of one another
+
+class Solution 
+{
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) 
+    {
+        // if both nodes are null, then it's a match
+        if (!p && !q) { return true; }
+        if (!p || !q) { return false; }
+
+        if (!isSameTree(p->left, q->left) || !isSameTree(p->right, q->right)) 
+        { 
+            return false; 
+        }
+        
+        return p->val == q->val;
+    
+    }
+};
