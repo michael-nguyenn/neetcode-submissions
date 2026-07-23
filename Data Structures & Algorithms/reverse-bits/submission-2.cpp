500c9a1e25364d415b850a45c43c8e3f6c0e259d
Michael Nguyen <104321330+michael-nguyenn@users.noreply.github.com>
2026-07-23 09:12:05 -0400

    Add: reverse-bits - submission-2

---
+++ Data Structures & Algorithms/reverse-bits/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution 
+{
+public:
+    uint32_t reverseBits(uint32_t n) 
+    {
+        uint32_t res = 0;
+
+        for (uint32_t i = 0; i < 32; i++)
+        {
+            uint32_t bit = (n >> i) & 1;
+            res = (bit << (31 - i)) | res;
+        }
+        return res;
+        
+    }
+};
