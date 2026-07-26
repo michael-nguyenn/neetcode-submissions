7804f930ab2ce85ed5a2c353cc0470de7a121976
Michael Nguyen <104321330+michael-nguyenn@users.noreply.github.com>
2026-07-26 12:19:09 -0400

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -0,0 +1,23 @@
+# Stack contains all temperatures along with their positions in temperatures 
+# in monotonic decreasing order.
+# Anytime we find a new daily temperature that is higher than our old temp at the 
+# top of our stack, we can pop it. The difference in idices is the amount of days
+# between the two temperatures.
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack: tuple[int, int] = [] # temp, index
+
+        for i, cur_temp in enumerate(temperatures):
+        
+            while stack and stack[-1][0] < cur_temp:
+                t, idx = stack.pop()
+                res[idx] = i - idx
+            
+            stack.append((cur_temp, i))
+
+        return res
+
+
+        
