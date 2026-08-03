e9f734932a210c62ed4409657c8b76e9cafd37a3
Michael Nguyen <104321330+michael-nguyenn@users.noreply.github.com>
2026-08-03 09:58:06 -0400

    Add: dota2-senate - submission-2

---
+++ Data Structures & Algorithms/dota2-senate/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        arr = list(senate)
+        print(arr)
+
+        while True:
+            for i in range(len(arr)):
+                if arr[i] == 'X': 
+                    continue
+                
+                j = (i + 1) % len(arr)
+                while j != i and (arr[j] == arr[i] or arr[j] == 'X'):
+                    j = (j + 1) % len(arr)
+                
+                if j == i:
+                    return "Dire" if arr[i] == 'D' else "Radiant"
+                else:
+                    arr[j] = 'X'
+            
+
