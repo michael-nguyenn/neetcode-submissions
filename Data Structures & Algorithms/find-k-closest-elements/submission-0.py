4b69fa39513c03443a032cf3d71a6423851f0507
Michael Nguyen <104321330+michael-nguyenn@users.noreply.github.com>
2026-08-21 08:24:10 -0400

    Add: find-k-closest-elements - submission-0

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-0.py
@@ -0,0 +1,20 @@
+import heapq
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        heap = []
+        res = []
+
+        for num in arr:
+            heapq.heappush(heap, (-(abs(x - num)), -num))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        while heap:
+            _, num = heapq.heappop(heap)
+            res.append(-num)
+        
+        res.sort()
+        return res
+        
