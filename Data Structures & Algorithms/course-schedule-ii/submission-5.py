ea14560512413d8731c0892ef81bc3c69c53b8d2
Michael Nguyen <104321330+michael-nguyenn@users.noreply.github.com>
2026-08-24 09:38:57 -0400

    Add: course-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-5.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        res = []
+        adj = {n:[] for n in range(numCourses)}
+        visit, complete = set(), set()
+
+        # guild our adj list
+        for course, pre in prerequisites:
+            adj[course].append(pre)
+
+        def dfs(course):
+            if course in visit:
+                return False
+            
+            if len(adj[course]) == 0:
+                res.append(course)
+                visit.add(course)
+                complete.add(course)
+                return True
+            
+            visit.add(course)
+
+            # otherwise go through the children
+            for pre in adj[course]:
+                if pre in complete:
+                    continue
+                
+                if not dfs(pre):
+                    return False
+            
+            # making it here means we've cleared the prereqs
+            res.append(course)
+            complete.add(course)
+            return True
+
+
+        for course in adj:
+            if course not in complete:
+                if not dfs(course):
+                    return []
+
+        return res
