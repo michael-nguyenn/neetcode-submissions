6dd6c4354987e587f76e3d6b27401c9ad8e9ccda
Michael Nguyen <104321330+michael-nguyenn@users.noreply.github.com>
2026-09-03 08:54:21 -0400

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        s1_map = {}
+        s2_map = {}
+        
+        # Build Hash Map
+        for i in range(len(s1)):
+            s1_map[s1[i]] = 1 + s1_map.get(s1[i], 0)
+            s2_map[s2[i]] = 1 + s2_map.get(s2[i], 0)
+        
+        if s1_map == s2_map:
+            return True
+
+        for i in range(len(s1), len(s2)):
+            # need to remove previous entry
+            to_remove = i - len(s1)
+            s2_map[s2[to_remove]] -= 1
+            if s2_map[s2[to_remove]] == 0:
+                del s2_map[s2[to_remove]]
+            
+            # then add current entry
+            s2_map[s2[i]] = 1 + s2_map.get(s2[i], 0)
+
+            if s1_map == s2_map:
+                return True
+        
+        return False
