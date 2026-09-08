cf39380b5232145084a1b7476a24a5de8c154d20
Michael Nguyen <104321330+michael-nguyenn@users.noreply.github.com>
2026-09-08 08:32:08 -0400

    Add: depth-of-binary-tree - submission-3

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-3.py
@@ -0,0 +1,22 @@
+# post order since we have to compute the longer of the two children
+# we can get the height of the children, then return 1 + max(children)
+# to cascade a result up
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        if not root:
+            return 0
+        
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+
+        return 1 + max(left, right)
+        
