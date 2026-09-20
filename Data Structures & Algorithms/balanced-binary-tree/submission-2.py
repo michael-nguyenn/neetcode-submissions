48a45a949b6cb125b98489289eddfb1e3093a9f7
Michael Nguyen <104321330+michael-nguyenn@users.noreply.github.com>
2026-09-20 09:27:17 -0400

    Add: balanced-binary-tree - submission-2

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-2.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# how do i cascade a boolean and the height?
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+
+        def dfs(node):
+            if not node:
+                return 0
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            if abs(left - right) > 1 or left == -1 or right == -1:
+                return -1
+
+            return 1 + max(left, right)
+
+        return dfs(root) != -1
+
+
+     
+        
+        
+        
